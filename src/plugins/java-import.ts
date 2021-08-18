d72fb68192515597c11b7aaef9797019ebe2c6cc
“coder1” <c.o.d.e.r.1@mail.ru>
2021-08-18 21:39:20 +0400

    panel 35%

--- src/plugins/java-import.ts
+++ src/plugins/java-import.ts
@@ -2,5 +2,7 @@
 import './range-slider/range-slider.scss';
 import './range-slider/range-slider.ts';
 
+// import './google-code-prettify/prettify.css';
+// import './google-code-prettify/prettify.js';
 
 
