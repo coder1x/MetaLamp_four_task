32b38ecf2f0fe6e9afef7f06f0248d1284355739
“coder1” <c.o.d.e.r.1@mail.ru>
2021-08-19 21:45:53 +0400

    added new components to the panel

--- src/components/code/code.ts
+++ src/components/code/code.ts
@@ -1,1 +1,48 @@
 import './code.scss';
+
+class CopyCode {
+
+  className: string;
+  elem: HTMLElement;
+  button: HTMLButtonElement;
+  code: HTMLElement;
+
+  constructor(className: string, component: HTMLElement) {
+    this.className = className;
+    this.elem = component;
+    this.setDomElem();
+    this.setActions();
+  }
+
+  setDomElem() {
+    this.button = this.elem.querySelector(this.className + '__copy');
+    this.code = this.elem.querySelector(this.className + '__lang-java');
+  }
+
+  setActions() {
+    this.button.addEventListener('click', () => {
+      const text = this.code.innerText;
+      navigator.clipboard.writeText(text)
+        .then(() => {
+        })
+        .catch(err => {
+          console.log('Something went wrong', err);
+        });
+    });
+  }
+
+}
+
+
+//==========================================================================
+
+function renderCopyCode(className: string) {
+  let components = document.querySelectorAll(className);
+  let objMas = [];
+  for (let elem of components) {
+    objMas.push(new CopyCode(className, elem as HTMLElement));
+  }
+  return objMas;
+}
+
+renderCopyCode('.code');
